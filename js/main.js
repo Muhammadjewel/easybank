4096052db3dd9c61c867ed4ad3053c38d8b4821d
muhammadjewel <msuratov@gmail.com>
2020-10-02 09:55:07 +0500

    Add nojs class removal scripts

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var elPageBody = document.querySelector('.page__body');
+var elSiteHeader = document.querySelector('.site-header');
+
+elPageBody.classList.remove('page__body--nojs')
+elSiteHeader.classList.remove('site-header--nojs')
