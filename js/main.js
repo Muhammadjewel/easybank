65b0b8c15997f8c9dd0f0227117838ee34a3aedd
muhammadjewel <msuratov@gmail.com>
2020-10-02 10:02:42 +0500

    Add sitenav JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 var elPageBody = document.querySelector('.page__body');
 var elSiteHeader = document.querySelector('.site-header');
 
-elPageBody.classList.remove('page__body--nojs')
-elSiteHeader.classList.remove('site-header--nojs')
+elPageBody.classList.remove('page__body--nojs');
+elSiteHeader.classList.remove('site-header--nojs');
+
+var elSiteHeaderToggler = elSiteHeader.querySelector('.site-header__toggler');
+
+elSiteHeaderToggler.addEventListener('click', function () {
+  elSiteHeader.classList.toggle('site-header--open');
+});
